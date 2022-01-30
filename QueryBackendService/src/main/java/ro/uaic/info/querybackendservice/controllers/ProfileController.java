bbc91f96b860e686a616e91a83674e7e535e5fe2
Adrian Hamod <hamoda@amazon.com>
2022-01-30 23:35:23 +0200

    adding id to beverage context

--- QueryBackendService/src/main/java/ro/uaic/info/querybackendservice/controllers/ProfileController.java
+++ QueryBackendService/src/main/java/ro/uaic/info/querybackendservice/controllers/ProfileController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.eclipse.rdf4j.model.IRI;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -27,6 +28,7 @@ import java.util.List;
 
 import static org.eclipse.rdf4j.model.util.Values.iri;
 
+@CrossOrigin(origins = "http://localhost:4200/", maxAge = 3600)
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/profiles")
@@ -81,6 +83,8 @@ public class ProfileController {
             @PathVariable String profileId,
             @RequestBody BeverageContext beverageContext) {
         IRI profileIri = iri(IRILabel.NS, profileId);
+        beverageContext.setId(iri(IRILabel.NS,
+                profileId + beverageContext.getBeverage().getLocalName() + "Context"));
         Profile profile = profileService.addBeverageContext(
                 profileIri,
                 beverageContext
